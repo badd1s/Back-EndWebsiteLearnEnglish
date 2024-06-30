40042a169f798dc13775eca4494b23f900597feb
badd1s <dang.hoangthe2209.com>
2024-07-01 03:03:54 +0700

    back-end english website

---
+++ controllers/system/registerController.js
@@ -0,0 +1,38 @@
+const User = require('../../models/user/User');
+const bcrypt = require('bcrypt');
+
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+
+    // kiểm tra trùng lặp
+    const duplicate = await User.findOne({ username: user }).exec();
+    if (duplicate) return res.sendStatus(409); //Conflict 
+
+    try {
+        //encrypt the password
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        const randomNumber = Math.floor(Math.random() * 100000); // Tạo số ngẫu nhiên 
+        const name = `User${randomNumber}`;
+
+        const imgUrl = 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/boy-icon.png'
+
+        const result = await User.create({
+            username: user,
+            password: hashedPwd,
+            avatar: imgUrl,
+            name: name
+        });
+
+        console.log(result);
+
+        res.status(201).json({ 'success': `New user ${user} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+}
+
+
+module.exports = {
+    handleNewUser
+};
